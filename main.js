9d0ead26d5b8fbb75ae6ae434ee344a2fe2c7249
SVS345 <93098341+SVS345@users.noreply.github.com>
2022-02-11 16:33:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+    fabric.Image.fromURL('birthdayImage.jpg',function(img){
+block_image_object = img;
+
+block_image_object.scaleToWidth(700);
+block_image_object.scaleToHeight(500);
+block_image_object.set({
+   top:0,
+   left:0,
+});
+canvas.add(block_image_object);
+    });
+}
+function playSound(){
+    x.playSound();
+	
+}
